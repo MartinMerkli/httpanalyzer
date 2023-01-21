3f280deb8019f86aa6f2441b9f6fbd1828de8135
Martin Merkli <martin.merkli@protonmail.com>
2023-01-21 20:37:48 +0100

    Rename argument/variable
    Rename argument/variable to reflect the fact that it is a list.

--- src/httpanalyzer/__init__.py
+++ src/httpanalyzer/__init__.py
@@ -2,20 +2,20 @@
 
 class Request:
 
-    def __init__(self, http_headers: dict, ip: str, path: str, admin_page: list = None) -> None:
+    def __init__(self, http_headers: dict, ip: str, path: str, admin_pages: list = None) -> None:
         """
         Class for analyzing http traffic from the http headers and the ip address.
         :param http_headers: dictionary of the http headers
         :param ip: the ip address (preferably IPv4, but IPv6 works as well)
         :param path: the full path (beginning with /) the request tried to access
-        :param admin_page: all different admin tools you are using (see README.md for more information)
+        :param admin_pages: all different admin tools you are using (see README.md for more information)
         """
-        if admin_page is None:
-            admin_page = []
+        if admin_pages is None:
+            admin_pages = []
         self._http_headers = http_headers
         self._ip = ip
         self._path = path
-        self._admin_page = admin_page
+        self._admin_pages = admin_pages
 
         self._malicious_rating = None
         self._bot_rating = None
