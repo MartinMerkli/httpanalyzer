19d68d5d529eafc86068a6ed3426c6b60ecdc426
Martin Merkli <martin.merkli@protonmail.com>
2023-01-22 09:56:01 +0100

    Move user agent data to different file
    Move user agent data of bots to different file

--- src/httpanalyzer/__init__.py
+++ src/httpanalyzer/__init__.py
@@ -21,70 +21,11 @@ class Request:
         self._bot_rating = None
 
     def bot(self) -> float:
+        from .src import BOTS_AGENT
         yes = 0.0
         no = 1.0
         user_agent = self._http_headers.get('user_agent', '').lower()
-        ua_blacklist = {
-            'abuse.xmco.fr',  # company providing cyber-security monitoring
-            'adidxbot',  # search engine
-            'adsbot-google',  # search engine
-            'alexa site audit',  # crawler
-            'apache-httpclient',  # client for java
-            'apis-google',  # search engine
-            'baiduspider',  # search engine
-            'bingbot',  # search engine
-            'bingpreview',  # search engine
-            'bit.ly/3ezndno',  # company creating statistics about server software
-            'censysinspect',  # company providing cyber-security monitoring
-            'cfnetwork',  # client for swift
-            'cyberwarcon',  # was trying to access the same uncommon url in regular intervals
-            'domains project',  # project creating statistics about domain names
-            'expanse, a palo alto networks company',  # company searching for specific information
-            'fasthttp',  # client for go
-            'feedfetcher-google',  # search engine
-            'fuzz faster u fool',  # automatic web fuzzer and pentesting tool
-            'github.com/projectdiscovery/httpx',  # web scraper
-            'go-http-client',  # client for go
-            'google-read-aloud',  # search engine
-            'google-site-verification',  # search engine
-            'googlebot',  # search engine
-            'hello world',  # generic bot user agent
-            'hello, momentum',  # generic bot user agent
-            'hello, umad?',  # generic bot user agent
-            'hello, world',  # generic bot user agent
-            'http banner detection',  # research project conducting network security studies
-            'https://csirt.divd.nl/',  # automatic vulnerability disclosure
-            'https://developers.google.com/',  # search engine
-            'https://gdnplus.com',  # crawler
-            'https://github.com/imroc/req'  # client for go
-            'internetmeasurement',  # automatic vulnerability disclosure
-            'java/',  # java programming language
-            'l9explore',  # pentesting tool
-            'l9tcpid',  # tool for analyzing connections
-            'libcurl',  # client for a variety of programming languages
-            'libwww-perl',  # client for perl
-            'masscan',  # web scraper
-            'mediapartners-google',  # search engine
-            'microsoftpreview',  # crawler
-            'msnbot-media',  # search engine
-            'netcraftsurveyagent',  # company providing cyber-security monitoring
-            'netsystemsresearch',  # research project conducting cybersecurity studies
-            'nmap',  # pentesting tool
-            'node.js',  # javascript programming language
-            'nvdorz',  # was trying to access the same sketchy url in regular intervals
-            'pandalytics',  # company monitoring the internet
-            'project-resonance',  # research project studying the security state of the internet
-            'project_patchwatch'  # research project conducting network security studies
-            'python',  # python programming language
-            'report runner',  # suddenly changed user agent from node.js to this one
-            'requests',  # client for python
-            'scaninfo@paloaltonetworks.com',  # company searching for specific information
-            'storebot-google',  # search engine
-            'tchelebi',  # company providing cyber-security monitoring
-            'urllib',  # client for python
-            'zgrab',  # pentesting tool
-        }
-        for element in ua_blacklist:
+        for element in BOTS_AGENT:
             if element in user_agent:
                 yes += 10.0
         return yes / (yes + no)
