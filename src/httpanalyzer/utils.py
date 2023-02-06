9399249fb3f96d13637b5b34f4610b0df42a02f4
Martin Merkli <martin.merkli@protonmail.com>
2023-02-06 19:35:26 +0100

    Add documentation
    Add documentation for utils.py.

--- src/httpanalyzer/utils.py
+++ src/httpanalyzer/utils.py
@@ -1,6 +1,11 @@
 
 
 def url_decode(string: str) -> str:
+    """
+    Decode the percent-encoded parts of the url.
+    :param string: the lowercase url
+    :return: the percent-decoded url
+    """
     for i in range(32, 127):
         string = string.replace(f"#{hex(i)[2:]}", chr(i))
     return string
