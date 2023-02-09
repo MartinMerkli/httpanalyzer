c34e8063dd1c9c75aff5337aa7159a3f93259fe9
Martin Merkli <martin.merkli@protonmail.com>
2023-02-09 14:37:18 +0100

    Make decoder recursive
    Make the url-decoder recursive

--- src/httpanalyzer/utils.py
+++ src/httpanalyzer/utils.py
@@ -6,6 +6,10 @@ def url_decode(string: str) -> str:
     :param string: the lowercase url
     :return: the percent-decoded url
     """
+    modified_string = string
     for i in range(32, 127):
-        string = string.replace(f"#{hex(i)[2:]}", chr(i))
-    return string
+        modified_string = modified_string.replace(f"#{hex(i)[2:]}", chr(i))
+    if modified_string != string:
+        return url_decode(modified_string)
+    else:
+        return modified_string
