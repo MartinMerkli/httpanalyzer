d1a71d141339dae7ea78bd83e36335b7e687edb7
Martin Merkli <martin.merkli@protonmail.com>
2023-04-29 11:41:20 +0200

    Move import statements
    Move import statements to the top of the file instead of each function.

--- src/httpanalyzer/__init__.py
+++ src/httpanalyzer/__init__.py
@@ -1,3 +1,6 @@
+from .src import MALICIOUS_PATHS as _MALICIOUS_PATHS, BOTS_PATH as _BOTS_PATH, BOTS_AGENT as _BOTS_AGENT
+from .src import SEARCH_ENGINE_AGENT as _SEARCH_ENGINE_AGENT
+from .utils import url_decode as _url_decode
 
 
 class Request:
@@ -27,14 +30,13 @@ class Request:
         :return: float between 0.0 and 1.0 (NOT linear distribution)
         """
         if self._bot_rating is None:
-            from .src import BOTS_AGENT, BOTS_PATH
             yes = 0.0
             no = 1.0
             user_agent = self._http_headers.get('User-Agent', '').lower()
-            for element in BOTS_AGENT:
+            for element in _BOTS_AGENT:
                 if element in user_agent:
                     yes += 10.0
-            if self._path in BOTS_PATH:
+            if self._path in _BOTS_PATH:
                 yes += 5.0
             if self._http_headers.get('Referer', '') != '':
                 no += 2.0
@@ -47,14 +49,13 @@ class Request:
         :return: float between 0.0 and 1.0 (NOT linear distribution)
         """
         if self._search_rating is None:
-            from .src import SEARCH_ENGINE_AGENT, BOTS_PATH
             yes = 0.0
             no = 1.0
             user_agent = self._http_headers.get('User-Agent', '').lower()
-            for element in SEARCH_ENGINE_AGENT:
+            for element in _SEARCH_ENGINE_AGENT:
                 if element in user_agent:
                     yes += 10.0
-            if self._path in BOTS_PATH:
+            if self._path in _BOTS_PATH:
                 yes += 0.2
             self._search_rating = yes / (yes + no)
         return self._search_rating
@@ -65,12 +66,10 @@ class Request:
         :return: float between 0.0 and 1.0 (NOT linear distribution)
         """
         if self._malicious_rating is None:
-            from .src import MALICIOUS_PATHS
-            from .utils import url_decode
             yes = 0.0
             no = 1.0
-            path = url_decode(self._path.lower())
-            for element in MALICIOUS_PATHS:
+            path = _url_decode(self._path.lower())
+            for element in _MALICIOUS_PATHS:
                 excluded = False
                 for page in self._admin_pages:
                     if page in element:
